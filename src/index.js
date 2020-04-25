ee137413bba0ce1e45b15787045f4bbe68992566
henry <henry.onyango@outlook.com>
2020-04-26 00:17:59 +0300

    Add redux provider to app root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
 //styles
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,9 +11,13 @@ import "./index.css";
 // components
 import App from "./components/App";
 
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
