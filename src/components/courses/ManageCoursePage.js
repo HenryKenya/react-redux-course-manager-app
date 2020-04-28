6a29305b86c0f23ea8f879958e0627c161d99f62
henry <henry.onyango@outlook.com>
2020-04-28 06:49:05 +0300

    Enable editing existing course

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -21,12 +21,14 @@ function ManageCoursePage({
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => console.log(error));
+    } else {
+      setCourse({ ...props.course });
     }
 
     if (authors.length === 0) {
       loadAuthors().catch((error) => console.log(error));
     }
-  }, []);
+  }, [props.course]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -62,9 +64,19 @@ ManageCoursePage.propTypes = {
   history: PropTypes.object.isRequired,
 };
 
-function mapStateToProps(state) {
+// selector function. Prefer memoization and have it in reducer
+function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors,
   };
